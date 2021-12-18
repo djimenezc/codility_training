7b7528f6109f29cfb97fb3998859b83cc1acfcdf
david <david.jimenez@nexthink.com>
2021-12-18 17:16:39 +0100

    mocha test working

---
+++ test/arrayRotation.specs.js
@@ -0,0 +1,14 @@
+import {expect} from 'chai';
+
+describe('example tests for nathan', () => {
+  it('should merge two JSONs that do not have any property in common ', () => {
+    const result = 1 === 1
+    expect(result).to.eql(true);
+  });
+
+  it('should merge two JSONs that do not have any property in common ', () => {
+    const result = 1 === 2
+    expect(result).to.eql(false);
+  });
+
+});
